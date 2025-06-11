2e56d547b69ffaea6d8c0e307527582a4ffeea8e
Jalon-Tech <71904112+Jalon-Tech@users.noreply.github.com>
2025-06-11 00:48:55 -0400

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,19 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBA7aIUj919ub7QtkQkAp-xv4XC0tzmSCI",
+  authDomain: "gamesync-union.firebaseapp.com",
+  projectId: "gamesync-union",
+  storageBucket: "gamesync-union.appspot.com",
+  messagingSenderId: "488911705365",
+  appId: "1:488911705365:web:ad8b3be7936a46bb77c51c"
+};
+
+const app = initializeApp(firebaseConfig);
+
+// 🔁 Make auth and db available globally
+window.auth = getAuth(app);
+window.db = getFirestore(app);
